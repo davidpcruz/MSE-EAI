898b00519f3c5463d1e979545bdd2b7c05e99d98
fjdn <NB12588@LT116671.novabase.intra>
2012-09-23 22:38:26 +0100

    Change: Updated implementation

--- Project1/src/eai/msejdf/apps/WebProbe.java
+++ Project1/src/eai/msejdf/apps/WebProbe.java
@@ -1,8 +1,15 @@
 package eai.msejdf.apps;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Date;
 
 import eai.msejdf.web.ParseStocksPlugin;
 import eai.msejdf.web.Parser;
@@ -37,7 +44,7 @@ public class WebProbe
 	{
 		// Expected call syntax: "java WebProbe <web url>"
 		
-		if (0 == args.length())
+		if (0 == args.length)
 		{
 			printHelp();			
 			System.exit(-1);			
@@ -60,41 +67,73 @@ public class WebProbe
 		this.webUrl = url;
 	}
 	
-	public void run()
+	public void run() throws Exception
 	{
 		Parser webParser = new ParseStocksPlugin(this.webUrl); 
 
 		webParser.parse(); //TODO: this has a return type. Handle it
 		
-		//TODO: Convert to xml
+		String message = this.createMessage(); //TODO: Implement this function with marshaling 
 		
-		// TODO: Write pending requests to topic
-		
-		// TODO: Write xml to topic. If down, write to pending requests file
-		
-	}
-	
-	private void writePendingMessages()
-	{
-		File[] fileList = getFileList(this.DIRECTORY__PENDING_MESSAGES);
-	
 		try
 		{
-			for (File messageFile : fileList)
-			{
-				// TODO: Write file contents
-				// TODO: Eliminate file if write OK
-			}
+			// As we may have messages that were previously not delivered, we''l try to send them first to 
+			// keep the same order
+			this.writePendingMessages();
+
+			// Now, dispatch our message
+			this.writeMessage(message); //TODO:Replace this by JMS API
 		}
 		catch(Exception exception)
 		{
-			// TODO: 
+			// As the dispatching of the message (or pending messages) failed, we'll save a local backup 
+			// and retry on the next run
+			this.saveMessageAsPending(message);
+		}		
+	}
+	
+	private void writeMessage(String message)
+	{
+		// TODO: This is a dummy function
+		
+		System.out.println("Message: " + message);
+	}
+	
+	private void writePendingMessages() throws IOException
+	{
+		// Pending messages to write are each in a file in a dedicated directory. Retrieve pending file list
+		File[] fileList = this.getFileList(this.DIRECTORY__PENDING_MESSAGES);
+	
+		for (File messageFile : fileList)
+		{
+			// Retrieve the message saved in the file
+			BufferedReader fileReader= new BufferedReader(new FileReader(messageFile));
+			String message = fileReader.readLine();
+			fileReader.close();
+
+			// Dispatch the message
+			this.writeMessage(message);
+
+			// At this point, the dispatch operation was successful, so eliminate file, as we don't need to send this message again
+			messageFile.delete();
 		}
 	}
 	
-	private void saveMessageAsPending(String message)
+	private void saveMessageAsPending(String message) throws IOException
 	{
+		File outputFile = null; 
 		
+		// Create a unique file  based on the current time/date
+		do
+		{
+			outputFile = new File(this.DIRECTORY__PENDING_MESSAGES + "/" + (new Date()).toString() + ".xml");
+		}
+		while (false == outputFile.createNewFile());
+
+		// Save the message to the file
+		BufferedWriter fileWriter = new BufferedWriter(new FileWriter(outputFile));
+		fileWriter.write(message);
+		fileWriter.close();
 	}
 	
 	private File[] getFileList(String directoryPath)
@@ -102,7 +141,7 @@ public class WebProbe
 		File directory = new File(directoryPath);
 		File[] fileList = directory.listFiles();
 
-		// Ensure return file list is ordered chronologically
+		// Ensure returned file list is ordered chronologically, so it is processed in the correct order
 		Arrays.sort(fileList, new Comparator<File>() 
 			{
 				public int compare(File file1, File file2)
@@ -113,6 +152,10 @@ public class WebProbe
 			});
 		
 		return fileList;
-	}
+	}	
 	
+	private String createMessage() //TODO: This will have a parameter with the object with the data
+	{
+		return "test message"; //TODO: Replace this!!!
+	}
 }
