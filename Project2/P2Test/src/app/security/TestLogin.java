978999e4cd6b2d93d5ef8552db00b259cfee14cf
fjdn <NB12588@LT116671.novabase.intra>
2012-11-04 17:05:50 +0000

    Add: Test project

---
+++ Project2/P2Test/src/app/security/TestLogin.java
@@ -0,0 +1,34 @@
+package app.security;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import eai.msejdf.exception.SecurityException;
+import eai.msejdf.security.ISecurity;
+
+public class TestLogin {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws NamingException, IOException, SecurityException {
+		
+		InitialContext ctx = new InitialContext();
+		ISecurity bean = (ISecurity) ctx.lookup("");
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Username: ");
+		String user = br.readLine();
+		
+		System.out.println("Password: ");
+		String pass = br.readLine();
+		
+		bean.Login(user, pass);
+	}
+
+}
