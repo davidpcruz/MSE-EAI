c152568735d2e09f0568bc85f94318c022a8a114
joaofcribeiro <joaofcribeiro@gmail.com>
2012-11-04 17:32:59 +0000

    .

---
+++ Project2/P2Test/src/app/security/TestRegister.java
@@ -0,0 +1,41 @@
+package app.security;
+
+import helper.Resource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import eai.msejdf.exception.SecurityException;
+import eai.msejdf.security.ISecurity;
+
+public class TestRegister {
+
+	/**
+	 * @param args
+	 * @throws NamingException 
+	 * @throws IOException 
+	 * @throws SecurityException 
+	 */
+	public static void main(String[] args) throws NamingException, IOException, SecurityException {
+		
+		InitialContext ctx = new InitialContext();
+		ISecurity bean = (ISecurity) ctx.lookup(Resource.JNDI_SECURIY_BEAN);
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Username: ");
+		String user = br.readLine();
+		
+		System.out.println("Password: ");
+		String pass = br.readLine();
+		
+		bean.RegisterUser(user, pass);
+		
+		System.out.println("User registered");
+	}
+
+}
---
+++ Project2/P2Test/src/helper/Resource.java
@@ -0,0 +1,6 @@
+package helper;
+
+public final class Resource {
+	public static final String JNDI_SECURIY_BEAN = "";
+
+}
