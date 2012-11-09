b467f9e05f6e3238373abf99f41e5e7cc3be2e92
joaofcribeiro <joaofcribeiro@gmail.com>
2012-11-09 02:15:19 +0000

    organize items

--- Project2/EJBServerInterfaces/ejbModule/eai/msejdf/user/IUserBean.java
+++ Project2/EJBServerInterfaces/ejbModule/eai/msejdf/user/IUserBean.java
@@ -10,32 +10,42 @@ import eai.msejdf.persistence.User;
 import javax.ejb.Remote;
 
 @Remote
-public interface  IUserBean {
-	public final static int SORT_BY_NAME = 0;	
-	
-	//public void addUser(User user);  // TODO: Move to ISecurity ?! 
-	//public void removeUser(User user);	// TODO: Move to ISecurity ?!
+public interface IUserBean {
+	public final static int SORT_BY_NAME = 0;
+
+	// public void addUser(User user); // TODO: Move to ISecurity ?!
+	// public void removeUser(User user); // TODO: Move to ISecurity ?!
 	public void updateUser(User user) throws ConfigurationException;
+
 	public User getUser(String name) throws ConfigurationException;
+
 	public User getUser(Long userid) throws ConfigurationException;
-	
+
 	public Company getCompany(Long userid) throws ConfigurationException;
-	public List<Company> getCompanyList(String filterPattern);	
-	public List<String> getCompanyNameList(String filterPattern);	
-	
-	public void followCompany(Long userid, Long companyId) throws ConfigurationException;
-	public void unfollowCompany(Long userid, Long companyId) throws ConfigurationException;
-	public List <Company> getfollowedCompanyList(Long id) throws ConfigurationException;
-	
-	public void setBankTeller(Long userid, Long companyId) throws ConfigurationException;
-	public void setBankTeller(Long userid, BankTeller bankTeller) throws ConfigurationException;
+
+	public List<Company> getCompanyList(String filterPattern);
+
+	public List<String> getCompanyNameList(String filterPattern);
+
+	public void followCompany(Long userid, Long companyId)
+			throws ConfigurationException;
+
+	public void unfollowCompany(Long userid, Long companyId)
+			throws ConfigurationException;
+
+	public List<Company> getfollowedCompanyList(Long id)
+			throws ConfigurationException;
+
+	public void setBankTeller(Long userid, Long companyId)
+			throws ConfigurationException;
+
+	public void setBankTeller(Long userid, BankTeller bankTeller)
+			throws ConfigurationException;
 
 	public BankTeller getBankTeller(Long userid) throws ConfigurationException;
-	public List<BankTeller> getBankTellerList(String filterPattern);
-	public List<String> getBankTellerNameList(String filterPattern);
-	public List<Company> getfollowedCompanyList2(Long userid)
-			throws ConfigurationException;
-	
 
-	
+	public List<BankTeller> getBankTellerList(String filterPattern);
+
+	public List<String> getBankTellerNameList(String filterPattern);
+
 }
