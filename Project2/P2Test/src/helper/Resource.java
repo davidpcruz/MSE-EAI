75fb2bd60ad660cbd5d6ba7ca3e9e3ac98054318
joaofcribeiro <joaofcribeiro@gmail.com>
2012-11-09 15:13:39 +0000

    Add Resource for IAdmin

--- Project2/P2Test/src/helper/Resource.java
+++ Project2/P2Test/src/helper/Resource.java
@@ -4,5 +4,8 @@ public final class Resource {
 	public static final String JNDI_SECURIY_BEAN = "P2EARDeploy/EJBServer/Security!eai.msejdf.security.ISecurity";
 	
 	public static final String JNDI_USER_BEAN = "P2EARDeploy/EJBServer/UserBean!eai.msejdf.user.IUserBean";
+	
+	public static final String JNDI_ADMIN_BEAN = "P2EARDeploy/EJBServer/Admin!eai.msejdf.admin.IAdmin";
+															  
 
 }
