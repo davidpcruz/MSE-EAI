908efb14d4d4c0396d9a44367c26def66349532a
joaofcribeiro <joaofcribeiro@gmail.com>
2012-11-14 23:58:43 +0000

    set timer

--- Project2/EJBServer/ejbModule/eai/msejdf/timer/DBCleaner.java
+++ Project2/EJBServer/ejbModule/eai/msejdf/timer/DBCleaner.java
@@ -41,7 +41,7 @@ public class DBCleaner {
 	 * @see eai.msejdf.timer.IDBCleanerRemote#cleanBankTeller()
 	 */
 
-	@Schedule(hour="*", minute="*", second="*/15")
+	@Schedule(hour = "*", minute = "*/15")
 	public void cleanBankTeller() {
 		if (logger.isDebugEnabled()) {
 			logger.debug("cleanBankTeller() - start"); //$NON-NLS-1$
@@ -49,33 +49,37 @@ public class DBCleaner {
 
 		List<Long> notUsedBankTellersId = new ArrayList<Long>();
 		// Get a list of all Bank Tellers in DB
-		Query queryGetBankTellersId = entityManager
-				.createQuery("SELECT bankTeller.id FROM  BankTeller AS bankTeller");
+		Query queryGetBankTellersId = entityManager.createQuery("SELECT bankTeller.id FROM  BankTeller AS bankTeller");
 		@SuppressWarnings("unchecked")
 		List<Long> completBankTellerListId = queryGetBankTellersId.getResultList();
 		// Get a list of the used Bank Tellers
-		Query queryGetUsedBankTellersId = entityManager
-				.createQuery("SELECT user.bankTeller.id FROM  User AS user");
+		Query queryGetUsedBankTellersId = entityManager.createQuery("SELECT user.bankTeller.id FROM  User AS user");
 		@SuppressWarnings("unchecked")
 		List<Long> usedBankTellerListId = queryGetUsedBankTellersId.getResultList();
+
 		
-		for(Long bankteler : completBankTellerListId){
-			if (!usedBankTellerListId.contains(bankteler)){
+		Query queryUnusedGetUsedBankTellersId = entityManager.createQuery("SELECT user.bankTeller.id FROM  User AS user");
+		@SuppressWarnings("unchecked")
+		List<Long> unusedGetUsedBankTellersId = queryUnusedGetUsedBankTellersId.getResultList();
+		logger.info("test query unused:" + unusedGetUsedBankTellersId);
+		
+		// Get the list of unused tBankTellerListId
+		for (Long bankteler : completBankTellerListId) {
+			if (!usedBankTellerListId.contains(bankteler)) {
 				notUsedBankTellersId.add(bankteler);
 			}
-
 		}
-		
-		if (0<notUsedBankTellersId.size()){
+
+		// If there are unused Bankteller so they should be deleted
+		if (0 < notUsedBankTellersId.size()) {
 			logger.info("Deleting  Bank Teller with ID:" + notUsedBankTellersId);
 			Query deleteQuery = entityManager.createQuery("delete from BankTeller where id in (:notUsedBankTellers) ");
 			deleteQuery.setParameter("notUsedBankTellers", notUsedBankTellersId);
 			deleteQuery.executeUpdate();
-		}else{
+		} else {
 			logger.info("cleanBankTeller speaking: No  Bank Teller are being unused.\n I'll skip for now but I will be back");
 		}
 
-			
 		if (logger.isDebugEnabled()) {
 			logger.debug("cleanBankTeller() - end"); //$NON-NLS-1$
 		}
