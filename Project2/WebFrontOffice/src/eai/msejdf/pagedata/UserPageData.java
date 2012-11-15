b16fccaf6f7327c26ccb5095ca19439e637ab50f
fjdn <NB12588@LT116671.novabase.intra>
2012-11-15 12:06:42 +0000

    BugFix: Allow correct bank teller and companies page refresh on actions BugFix: Changed error message check to properly test the error

--- Project2/WebFrontOffice/src/eai/msejdf/pagedata/UserPageData.java
+++ Project2/WebFrontOffice/src/eai/msejdf/pagedata/UserPageData.java
@@ -4,6 +4,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import eai.msejdf.exception.ConfigurationException;
+import eai.msejdf.persistence.Address;
 import eai.msejdf.persistence.User;
 import eai.msejdf.user.IUserBean;
 import eai.msejdf.utils.EJBLookupConstants;
@@ -13,13 +14,23 @@ public class UserPageData{
 	
 	private IUserBean userBean;
 	private User user;
-	
+	private Address address;
+
 	public UserPageData() throws NamingException, ConfigurationException {
 		InitialContext ctx = new InitialContext();
 		
 		this.userBean = (IUserBean) ctx.lookup(EJBLookupConstants.EJB_I_USER);
 		
 		this.user = this.userBean.getUser(SessionManager.getProperty(SessionManager.USERNAME_PROPERTY));
+		
+		if (null != this.user.getAddress())
+		{
+			this.address = this.user.getAddress();
+		}
+		else
+		{
+			this.address = new Address();
+		}
 	}
 
 	public User getUser() {
@@ -31,16 +42,20 @@ public class UserPageData{
 	}
 
 	public boolean updateUser() {
-		// TODO: The exception should cause the user interface to be informed
 		try {
 			this.userBean.updateUser(this.user);
 		} catch (Exception exception) {
 			exception.printStackTrace();
 			return false;
 		}
-
 		return true;
 	}
 
-	
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 }
