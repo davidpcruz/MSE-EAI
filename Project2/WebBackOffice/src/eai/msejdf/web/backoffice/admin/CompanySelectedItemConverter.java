1c8cd9fb74cd6bd053502457fbcc8a47ef3e9c9c
davidpcruz <david.cruz@gmail.com>
2012-11-15 02:43:16 +0000

    Completed the subcriptions list on the backoffice Added the sort to the admin bean

---
+++ Project2/WebBackOffice/src/eai/msejdf/web/backoffice/admin/CompanySelectedItemConverter.java
@@ -0,0 +1,25 @@
+package eai.msejdf.web.backoffice.admin;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+import eai.msejdf.persistence.Company;
+
+@ManagedBean(name="companySelectedConverter")
+public class CompanySelectedItemConverter implements Converter {
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        return ((Company) value).getId().toString();
+    }
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value)
+	{
+		Company toReturn = new Company();
+		toReturn.setId(Long.valueOf(value));
+		
+		return toReturn;
+	}
+}
