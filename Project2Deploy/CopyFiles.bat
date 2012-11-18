707b34efea6e6ed18c826910204f34b15219560d
davidpcruz <david.cruz@gmail.com>
2012-11-18 12:58:48 +0000

    Added script to creat zip file

--- Project2Deploy/CopyFiles.bat
+++ Project2Deploy/CopyFiles.bat
@@ -1,25 +1,54 @@
 @echo off
 :: ---------------------------------------------------------------
-:: Windows startup batch file for RDDDaemon!
+:: Prepare files for deployment
 :: Version 0.1
 ::
 :: Last Change: 
 :: ---------------------------------------------------------------
 
 
+:: EAR Source dir
+set EARSrc=..\Project2\P2EARDeploy\deploy\
+:: EAR Destination dir
+set EARDest=ear
 
-:: Site URL
-set siteUrl=  http://www.nextbolsa.com/cotacoes.php?action=psi20
-:: Parser Plugin
-set parserPlugin= eai.msejdf.web.ParseStocksPlugin
+:: SRC Source dir
+set JavaSrc=..\Project2\
+:: EAR Destination dir
+set JavaDest=src
 
+:: EAR Destination dir
+set SqlDest=sql
 
+:: Zip File
+set zipFile=P2Deploy.zip
 
 :: ===============================================================
 
 
-::java WebProve <Site URL> <Analysis Plugin>
-echo java  -cp  EAI-Proj1_v5.jar;lib/*  eai.msejdf.apps.WebProbe %siteUrl% %parserPlugin% 
-java  -cp EAI-Proj1_v5.jar;lib/*;. eai.msejdf.apps.WebProbe %siteUrl% %parserPlugin% 
+:: copy ear files
+echo copying EAR
+xcopy /I /Y %EARSrc%*.ear %EARDest%
+
+:: copy source files
+echo copying Source
+if exist %JavaDest% rmdir /s /q %JavaDest%
+
+xcopy /E /I %JavaSrc%\EAICommon %JavaDest%\EAICommon
+xcopy /E /I %JavaSrc%\EJBServer %JavaDest%\EJBServer 
+xcopy /E /I %JavaSrc%\EJBServerInterfaces %JavaDest%\EJBServerInterfaces
+xcopy /E /I %JavaSrc%\JPAData %JavaDest%\JPAData
+xcopy /E /I %JavaSrc%\P2EARDeploy %JavaDest%\P2EARDeploy
+xcopy /E /I %JavaSrc%\WebBackOffice %JavaDest%\WebBackOffice
+xcopy /E /I %JavaSrc%\WebFrontOffice %JavaDest%\WebFrontOffice
+
+:: copy ear files
+echo copying SQL
+if not exist %sqlDest% mkdir %sqlDest%
+
+:: Zipping it
+echo Zipping it
+if exist %zipFile% del zipFile
+7z.exe a %zipFile% %EARDest% %sqlDest% %JavaDest%
 
 pause
