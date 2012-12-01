8942f6f89042a71ebd8475999383e33cb634107d
davidpcruz <david.cruz@gmail.com>
2012-12-01 17:42:57 +0000

    Added new company to the project added the new ESB services project

---
+++ Project3/EAICommon/src/eai/msejdf/esb/Company.java
@@ -0,0 +1,287 @@
+package eai.msejdf.esb;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class Company {
+
+    protected String name;
+    protected String address;
+    protected String website;
+    
+    // stock values
+    protected BigDecimal lastQuotation;
+    protected String time;
+    protected BigDecimal variation;
+    protected BigInteger quantity;
+    protected BigDecimal maximum;
+    protected BigDecimal minimum;
+    protected BigDecimal purchase;
+    protected BigDecimal sell;
+
+    /**
+     * Gets the value of the name property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Gets the value of the address property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * Sets the value of the address property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setAddress(String value) {
+        this.address = value;
+    }
+
+    /**
+     * Gets the value of the website property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getWebsite() {
+        return website;
+    }
+
+    /**
+     * Sets the value of the website property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setWebsite(String value) {
+        this.website = value;
+    }
+
+
+    /**
+     * Gets the value of the lastQuotation property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public BigDecimal getLastQuotation() {
+        return lastQuotation;
+    }
+
+    /**
+     * Sets the value of the lastQuotation property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public void setLastQuotation(BigDecimal value) {
+        this.lastQuotation = value;
+    }
+
+    /**
+     * Gets the value of the time property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getTime() {
+        return time;
+    }
+
+    /**
+     * Sets the value of the time property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setTime(String value) {
+        this.time = value;
+    }
+
+    /**
+     * Gets the value of the variation property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public BigDecimal getVariation() {
+        return variation;
+    }
+
+    /**
+     * Sets the value of the variation property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public void setVariation(BigDecimal value) {
+        this.variation = value;
+    }
+
+    /**
+     * Gets the value of the quantity property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigInteger }
+     *     
+     */
+    public BigInteger getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * Sets the value of the quantity property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigInteger }
+     *     
+     */
+    public void setQuantity(BigInteger value) {
+        this.quantity = value;
+    }
+
+    /**
+     * Gets the value of the maximum property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public BigDecimal getMaximum() {
+        return maximum;
+    }
+
+    /**
+     * Sets the value of the maximum property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public void setMaximum(BigDecimal value) {
+        this.maximum = value;
+    }
+
+    /**
+     * Gets the value of the minimum property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public BigDecimal getMinimum() {
+        return minimum;
+    }
+
+    /**
+     * Sets the value of the minimum property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public void setMinimum(BigDecimal value) {
+        this.minimum = value;
+    }
+
+    /**
+     * Gets the value of the purchase property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public BigDecimal getPurchase() {
+        return purchase;
+    }
+
+    /**
+     * Sets the value of the purchase property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public void setPurchase(BigDecimal value) {
+        this.purchase = value;
+    }
+
+    /**
+     * Gets the value of the sell property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public BigDecimal getSell() {
+        return sell;
+    }
+
+    /**
+     * Sets the value of the sell property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public void setSell(BigDecimal value) {
+        this.sell = value;
+    }
+
+}
